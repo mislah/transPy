8f3469c303a51dfd252bedc6492ab7eb69e91a1d
Mislah <git@mislah.com>
2023-02-25 18:48:11 +0530

    Support storing tokens in JSON file
    The parser is modified to input the dictionary of tokens from a
    JSON file. The _tokens.py is converted to JSON format and renamed
    to _tokens.json.

--- _parser.py
+++ _parser.py
@@ -1,9 +1,10 @@
-import _tokens
+import json
+lib = json.load(open('_tokens.json', encoding = 'utf8'))
 
 def parse(path_in, path_out):
     with open(path_in, encoding = 'utf8') as inf:
         with open(path_out, 'w', encoding = 'utf8') as outf: 
             for i in inf:
-                for j in _tokens.lib:
-                    i = i.replace(j,_tokens.lib[j])
+                for j in lib:
+                    i = i.replace(j, lib[j])
                 outf.write(i)     

4cfe39179119bf08f4650f32b0746582c06a6995
Mislah <git@mislah.com>
2023-06-30 19:11:25 +0530

    Separate tokenstreams and strings from source code
    Previously, scn.py could only detect the position of strings and comments.
    This update enables scn.py to utilize the tokenizer library for separating
    token streams, strings, and comments from the source code. After obtaining
    the position of strings and comments using the tokenizer library, they are
    separated out and stored in an array of dictionaries with specified values
    and types, preserving the order.

--- str_test/scn.py
+++ str_test/scn.py
@@ -1,17 +1,33 @@
 from io import BytesIO
 import tokenize
 
-with open('test.py', 'r') as f:
-   data = f.read()
+with open('str_test/test.py', 'r') as f:
+   source = f.read()
 
-tokens = tokenize.tokenize(BytesIO(data.encode('utf-8')).readline) 
-source = list()
+# map offsets of each line from (0,0)
+pos = 0
+offset = dict()
+for i, x in enumerate(source.splitlines(keepends=True)):
+   offset[i+1] = pos
+   pos += len(x)
+
+# find position of strings
+strings = list()
+tokens = tokenize.tokenize(BytesIO(source.encode('utf-8')).readline) 
 for token in tokens:
-   if token.type == tokenize.STRING:
-      token = token._replace(string="$")
-   elif token.type == tokenize.COMMENT:
-      token = token._replace(string="#")
-   source.append(token)
+   if token.type == tokenize.STRING or token.type == tokenize.COMMENT:
+      start = offset[token.start[0]] + token.start[1]
+      end = offset[token.end[0]] + token.end[1]
+      strings.append((start, end))
 
-transp = tokenize.untokenize(source).decode('utf-8')
-print(transp)
+# separate source into tokenstreams and strings
+lines = []
+for start, end in strings[::-1]:
+   lines.append({'type': 'tstream', 'val': source[end:]})
+   lines.append({'type': 'sstream', 'val': source[start:end]})
+   source = source[:start]
+lines.append({'type': 'tstream', 'val': source})
+lines.reverse()
+
+for i in lines:
+   print(i)

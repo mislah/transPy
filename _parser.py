301f27dd4a02bb45ee91c7b4847b7aa439be017a
Mislah <git@mislah.com>
2023-07-03 17:20:21 +0530

    Use the new lexer to prevent transpiling strings
    Update the parser to use the new lexer, ensuring that only keywords and
    other fundamental constructs are transpiled. This prevents the possibility
    of mistakenly transpiling keywords within strings and comments.

--- _parser.py
+++ _parser.py
@@ -1,10 +1,12 @@
 import json
+import _lexer
 lib = json.load(open('_tokens.json', encoding = 'utf8'))
 
 def parse(path_in, path_out):
-    with open(path_in, encoding = 'utf8') as inf:
-        with open(path_out, 'w', encoding = 'utf8') as outf: 
-            for i in inf:
+    tokens = _lexer.lex(path_in)
+    with open(path_out, 'w', encoding = 'utf8') as outf: 
+        for i in tokens:
+            if i['type'] == 'tstream':
                 for j in lib:
-                    i = i.replace(j, lib[j])
-                outf.write(i)     
+                    i['val'] = i['val'].replace(j, lib[j])
+            outf.write(i['val'])     
